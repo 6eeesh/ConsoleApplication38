d4e22365045387796444ef2f78f7f132c4d388ce
6eeesh <144094732+6eeesh@users.noreply.github.com>
2023-12-18 14:33:58 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication38/ConsoleApplication38.cpp
@@ -0,0 +1,32 @@
+﻿﻿#include <iostream>
+#include <set>
+using namespace std;
+
+
+int main()
+{
+    set<int> Tree;
+
+    Tree.insert(14);
+    Tree.insert(10);
+    Tree.insert(9);
+    Tree.insert(27);
+    Tree.insert(21);
+
+    cout << "All tree elements:"<<endl;
+    for (const int& elements : Tree)
+    {
+        cout << elements << endl;
+    }
+
+    Tree.erase(10);
+    Tree.erase(27);
+
+    cout << "\nTree elements after removal:"<<endl;
+    for (const int& elements : Tree)
+    {
+        cout << elements << endl;
+    }
+
+    return 0;
+}
